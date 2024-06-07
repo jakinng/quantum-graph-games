baf04a184cbbd19832ed5889f695f31f526a6213
jakinng <jakin.ng03@gmail.com>
2024-06-07 14:23:51 -0400

    testing permissions

---
+++ toqito-example.py
@@ -0,0 +1,8 @@
+from toqito.states import basis, bell
+from toqito.state_props import is_ppt
+import numpy as np
+
+prob_mat = np.ones((2, 2)) * 1 / 4
+S, T, A, B = 2, 2, 2, 2
+pred_mat = np.zeros((S, T, A, B))
+print(np.indices(pred_mat.shape))
